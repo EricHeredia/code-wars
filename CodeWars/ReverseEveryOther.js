f6e3b3eb9a5368743f4bcf23f0455f189a630de8
Eric Heredia <ericdavidheredia@gmail.com>
2019-07-29 15:54:16 -0500

    Reverse Every Other Word In The String

---
+++ CodeWars/ReverseEveryOther.js
@@ -0,0 +1,10 @@
+//Reverse every other word in a given string, then return the string. 
+//Throw away any leading or trailing whitespace, while ensuring there is 
+//exactly one space between each word. Punctuation marks should be treated 
+//as if they are apart of the word in this kata.
+
+// First attempt - 2 minutes
+
+function reverse(str){
+    return str.split(' ').map((x,i) => i % 2 === 0 ? x : x.split('').reverse().join('')).join(' ')
+  }
