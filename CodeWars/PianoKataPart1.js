77e6054572786c40f977aed54076c3154be7d19a
EricHeredia <ericdavidheredia@gmail.com>
2019-08-13 18:39:38 -0500

    Piano Kata Part 1

--- CodeWars/PianoKataPart1.js
+++ CodeWars/PianoKataPart1.js
@@ -26,3 +26,14 @@ function blackOrWhiteKey(keyPressCount) {
         return 'black';
     }
 }
+
+// Cleaning look of previous
+
+function blackOrWhiteKey(keyPressCount) {
+    switch ((keyPressCount - 1) % 88 % 12) {
+      case 0: case 2: case 3: case 5: case 7: case 8: case 10:
+        return 'white';
+      default:
+        return 'black';
+    }
+}
